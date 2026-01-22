fffea63f69967c3fef6807352e0f3fdaa4e3df59
Minerva <superetro85@gmail.com>
2026-01-21 22:40:18 -0300

    study: undestaing strings

---
+++ JuanSoulie/6-string-literals-especial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Nothng more \n";
+    cout << "Nothing mre \t";
+    cout << '\n';
+    cout << '\t';
+    cout << "Left \t Right";
+    cout << "one\ntwo\nthree\n";
+    cout << "\40 or \23";
+    cout << "string expressed in \two lines";
+    cout << "Reino Blanco\t" "Kamiiro Awase\t" L"Tiny Light\t" "Secret Base\t"; 
+
+    return 0;
+}
